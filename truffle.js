2f9f70b6d5359ae18397542839d70096435f0e19
cyrsis <em3888@gmail.com>
2018-08-13 13:38:28 +0800

    Adding some more network parms

--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // for more about customizing your Truffle configuration!
@@ -7,6 +9,17 @@ module.exports = {
             port: 7545,
             gas: 6721974,
             network_id: "*" // Match any network id
+        },
+        ropsten: {
+            network_id: 3,
+            host: "localhost",
+            port: 8545,
+            gas: 2900000
+        },
+        "ropsten-infura": {
+            provider: () => new HDWalletProvider("<passphrase>", "https://ropsten.infura.io/<key>"),
+            network_id: 3,
+            gas: 4700000
         }
     },
 
@@ -16,12 +29,7 @@ module.exports = {
             runs: 200
         }
     },
-    ropsten: {
-        network_id: 3,
-        host: "localhost",
-        port: 8545,
-        gas: 2900000
-    }
+
 
 
 };
