db4fefd0877ca80f5e6a69b2db210fdfb1a1f4c9
cyrsis <em3888@gmail.com>
2018-08-17 13:42:31 +0800

    Confirm everything is working

--- truffle.js
+++ truffle.js
@@ -7,21 +7,21 @@ module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // for more about customizing your Truffle configuration!
     networks: {
-        // development: {
-        //     host: "127.0.0.1",
-        //     port: 7545,
-        //     // gas: 6721974,
-        //     network_id: "*"
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            // gas: 6721974,
+            network_id: "*"
+        }
+        // rinkeby: {
+        //     provider: function () {
+        //         return new HDWalletProvider(mnemonic,
+        //             "https://rinkeby.infura.io/v3/APIKEY");
+        //     },
+        //     gas: 4500000,
+        //     gasPrice: 25000000000,
+        //     network_id: "4"
         // },
-        rinkeby: {
-            provider: function () {
-                return new HDWalletProvider(mnemonic,
-                    "https://rinkeby.infura.io/v3/APIKEY");
-            },
-            gas: 4500000,
-            gasPrice: 25000000000,
-            network_id: "4"
-        },
         // kovan: {
         //     provider: new HDWalletProvider(mnemonic, 'https://kovan.infura.io'),
         //     network_id: '*',
